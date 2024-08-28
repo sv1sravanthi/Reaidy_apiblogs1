081bb07be9d4a8b5c0bed23a6fbb6971973e866f
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2024-08-28 21:33:36 +0530

    creating models

---
+++ models/blogs.js
@@ -0,0 +1,33 @@
+const { default: mongoose } = require("mongoose")
+
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type:String,
+      ref: 'User',
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = Blog;
